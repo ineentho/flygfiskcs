22d76e0268b1c22764162751dae7ee36250254a6
Henrik Karlsson <hk.henrik@gmail.com>
2015-06-26 12:27:01 +0200

    Connection messages + reordering

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,44 @@
 import test from './module'
 
+const feed = document.querySelector('.feed');
+
 console.log('asd');
+
 
 test();
 
-
 var socket = io('http://fj100.ineentho.com:3000/');
-
+function message(data) {
+    feed.innerHTML = data + '<br>' + feed.innerHTML;
+}
 socket.on('kill', function (data) {
-    document.querySelector('.feed').innerHTML += JSON.stringify(data) + '<br>';
+    message(JSON.stringify(data));
+});
+
+socket.on('connect', function() {
+    message('Connected');
+});
+
+socket.on('connect_error', function() {
+    message('Connection error');
+});
+
+socket.on('reconnect', function() {
+    message('Reconnected');
+});
+
+socket.on('reconnect_attempt', function () {
+    message('reconnect_attempt');
+});
+
+socket.on('reconnecting', function (n) {
+    message('reconnecting #' + n);
+});
+
+socket.on('reconnect_error', function () {
+    message('reconnect_error');
+});
+
+socket.on('reconnect_failed', function () {
+    message('reconnect_failed');
 });
