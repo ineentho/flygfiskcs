23a7549be0ef8ccf14845b780803649e21e4cb27
Henrik Karlsson <hk.henrik@gmail.com>
2015-06-26 19:53:34 +0200

    Changed deafult UL

--- app/js/main.js
+++ app/js/main.js
@@ -7,7 +7,7 @@ console.log('asd');
 
 test();
 
-const connectionUrl = window.location.hash.substr(1) || 'http://flygfiskcs-ineentho.rhcloud.com/';
+const connectionUrl = window.location.hash.substr(1) || 'http://flygfisk-stats.ineentho.com/';
 var socket = io(connectionUrl);
 message('Connecting to ' + connectionUrl);
 function message(data) {
